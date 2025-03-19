88cdba0f5c773f516843015fbd6e0fac83a30020
guilhermeabtibol <guiabtibol@gmail.com>
2025-03-19 19:32:27 -0400

    Incluindo Marca e Veiculo

---
+++ Aula4/src/Marca.java
@@ -0,0 +1,10 @@
+
+public class Marca {
+     String nome;
+
+     Marca(){}
+
+    Marca(String nome) {
+         this.nome = nome;
+    }
+}
---
+++ Aula4/src/Veiculo.java
@@ -0,0 +1,17 @@
+public class Veiculo {
+    public String modelo;
+    public String marca;
+    public String cor;
+    public Double valor;
+    public Double ipva;
+
+    public void calcIpva() {
+        if ( valor <= 40000.0) {
+            ipva = valor *2/100;
+        } else if(valor<70000.0) {
+            ipva = valor * 3/100;
+        } else {
+            ipva = valor * 5 / 100;
+        }
+    }
+}
